851f35b3f15c0198ccbe9b137d54413397b83b64
Tim Pilius <tpilius@gmail.com>
2023-06-14 10:30:08 -0400

    Adding sonicmode for wintermute

---
+++ Wintermute/SonicMode/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: MelonInfo(typeof(SonicMode.SonicMode), "SonicMode", "1.0", "tpill90")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+[assembly: AssemblyTitle("SonicMode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SonicMode")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a599078e-c03e-4b30-9eea-078fd54455eb")]
+
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
